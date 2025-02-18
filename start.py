6a2bc8585c59d07f1e95886830dc58479155ecb9
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-18 23:35:25 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,26 @@
+import os  
+from dotenv import load_dotenv
+import discord
+from discord import app_commands
+from discord.ext import commands
+import random
+from keep_alive import keep_alive
+import json
+import asyncio
+
+
+load_dotenv()
+
+
+token = os.getenv('TOKEN_BOT_DISCORD')
+
+# Intents et configuration du bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté en tant que {bot.user}")
+
+keep_alive()
+bot.run(token)
